d555a3a71311adb05a1940a9b21df135069c92ce
Akshay Shirsath <40054782+akshayonly@users.noreply.github.com>
2024-08-26 17:48:12 +0530

    drive link

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.warning('Site Under Development', icon="⚠️")
 st.divider()
 
 # Read data from a CSV file
-data = pd.read_csv("https://raw.githubusercontent.com/akshayonly/ProkaryoteComplexOneWebApp/main/data.csv")
+data = pd.read_csv("https://drive.google.com/uc?id=1xLDFvWmKhvwCb0DyNhqGUo5y8ZO_H1wx&export=download")
 
 # Slider for selecting the number of subunits
 subunits = st.slider("Subunits", 1, 14, 11)
